473efca78f1facda02089c77853cf9c6770c9cd1
hehorma1 <hehorma1@directv.cl>
2020-12-07 15:19:48 -0300

    class strucutred and added compatibility to sony and lider store

--- main.py
+++ main.py
@@ -2,195 +2,294 @@ import urllib.request
 from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from time import sleep
 
-def ps5_encontrada(temp_url="", temp_class=""):
-    webhook = open("webhook.txt", "r")
-    wh_url = webhook.read()
-    requests.get(url = wh_url)
-    print("\n\n #################################################found in: \n "+temp_url+"\n\n")
-    driver = webdriver.Firefox()
-    driver.get(temp_url)
-    sleep(1)
-    driver.find_element_by_class_name(temp_class).click() 
 
-def check_store(temp_url="", temp_class="", temp_id=""):
-    datos = urllib.request.Request(temp_url, headers={'User-Agent': 'Mozilla/5.0'}) 
-    web_byte = urllib.request.urlopen(datos).read()
-    try:
-        webpage = web_byte.decode('utf-8')
-    except:
-        webpage = web_byte
-    soup = BeautifulSoup(webpage)
-    results = soup.find_all(temp_id, class_=temp_class)
-    return results
+class ps5_sarch_tool:
+    def __init__(self):
 
+        self.temp_url =''
+        self.test_url =''
+        self.temp_class =''
+        self.driver =''
+        self.html=''
+        self.webpage=''
+        self.soup=''
+        self.id=''
 
+    def encontrada(self):
+        webhook = open("webhook.txt", "r")
+        wh_url = webhook.read()
+        requests.get(url = wh_url)
+        print("\n\n #################################################found in: \n "+self.temp_url+"\n\n")    
+        #driver = webdriver.Firefox()
+        #driver.get(temp_url)
+        #sleep(1)
+        self.driver.quit()
+        self.driver = webdriver.Firefox()
+        self.driver.minimize_window()
+        self.driver.get(self.temp_url)
+        sleep(1)
+        try:
+            if self.id=='':
+                self.driver.find_element_by_class_name(self.temp_class).click() 
+            else:
+                self.driver.find_element_by_id(self.id).click()
+        except:
+            pass
 
-for x in range(15):
-    try:
-        #PS5
-        store = "zmart"
-        temp_url = 'https://www.zmart.cl/scripts/prodView.asp?idProduct=79725'
-        test_url = 'https://www.zmart.cl/scripts/prodView.asp?idProduct=79724'
-        # temp_url = test_url
-        temp_class = 'comprar2015'
-        temp_id = 'input'
+    def check_store(self):
+        #datos = urllib.request.Request(temp_url, headers={'User-Agent': 'Mozilla/5.0'}) 
+        #web_byte = urllib.request.urlopen(datos).read()
+        
+        # self.temp_url = self.test_url
 
-        results = check_store(temp_url,temp_class,temp_id)
-        resultado = results[0]
-        #print(resultado)
-        ps5_encontrada(temp_url,temp_class)
-    except:
-        print("not found in "+store)
-        pass
-    try:
-        #PS5  
-        store = "ripley"  
-        temp_url = 'https://simple.ripley.cl/reserva-consola-ps5-digital-edition-2000380458314p'
-        test_url = 'https://simple.ripley.cl/control-ps5-dualsense-wireless-2000380632868p?color_80=blanco&cbar_widget_p4=&s=o'
-        # temp_url = test_url
-        temp_class = 'js-buy-button'
-        temp_id = 'button'
-
-        results = check_store(temp_url,temp_class,temp_id)
-        resultado = results[0]
-        #print(resultado)
-        if results[0].next == 'Agregar a la bolsa':
-            ps5_encontrada(temp_url,temp_class)
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        self.driver = webdriver.Firefox(options=options)
+        self.driver.get(self.temp_url)
+        sleep(1)
+        self.web_byte = self.driver.page_source
+        try:
+            self.webpage = self.web_byte.decode('utf-8')
+        except:
+            self.webpage = self.web_byte
+        self.soup = BeautifulSoup(self.webpage)
+        if self.id == '':
+            self.results = self.soup.find_all(self.temp_id, class_=self.temp_class)
         else:
-            print("not found in "+store)
-    except:
-        print("not found in "+store)
-    try:
-        #PS5   
-        store="microplay"
-        temp_url = 'https://www.microplay.cl/producto/consola-digital-ps5-sony/'
-        test_url = 'https://www.microplay.cl/producto/control-dualsense-ps5-wireless-white-sony/'
-        # temp_url = test_url
-        temp_class = 'btn-submit'
-        temp_id = 'a'
+            temp_list=[]
+            temp_list.append(ps5.driver.find_element_by_id(self.id))
+            self.results = temp_list
         
-        results = check_store(temp_url,temp_class,temp_id)
-        resultado = results[0]
-        #print(resultado)
-        ps5_encontrada(temp_url,temp_class)
-    except:
-        print("not found in "+store)
-    # try:
-    #     #PS5    
-    #     datos = urllib.request.Request('https://store.sony.cl/playstation5-digital/p', headers={'User-Agent': 'Mozilla/5.0'}) 
-    #     #datos = urllib.request.Request('https://store.sony.cl/ps5-control-inalambrico-dualsense/p', headers={'User-Agent': 'Mozilla/5.0'}) 
-    #     web_byte = urllib.request.urlopen(datos).read()
-    #     webpage = web_byte.decode('utf-8')
-    #     soup = BeautifulSoup(webpage)
-    #     results = soup.find_all('div', class_='box buy')
-    #     resultado = results[0]
-    #     print(resultado)
-    #     requests.get(url = 'https://maker.ifttt.com/trigger/PS5/with/key/FncyI5tezmj4m-Ti7CUPqUcFOqappribX3dZeXAE2g')
-    #     print("#################################################found in sony CL")
-    # except:
-    #     pass
-    # try:
-    #     #PS5    
-    #     datos = urllib.request.Request('https://store.sony.com.co/playstation5/p', headers={'User-Agent': 'Mozilla/5.0'}) 
-    #     #datos = urllib.request.Request('https://store.sony.cl/ps5-control-inalambrico-dualsense/p', headers={'User-Agent': 'Mozilla/5.0'}) 
-    #     web_byte = urllib.request.urlopen(datos).read()
-    #     webpage = web_byte.decode('utf-8')
-    #     soup = BeautifulSoup(webpage)
-    #     results = soup.find_all('a', class_='buy-button')
-    #     resultado = results[0]
-    #     print(resultado)
-    #     if not(str(resultado.text).find("Producto sin stock")):
-    #         requests.get(url = 'https://maker.ifttt.com/trigger/PS5/with/key/FncyI5tezmj4m-Ti7CUPqUcFOqappribX3dZeXAE2g')
-    #         print("#################################################found in sony CO")
-    # except:
-    #     pass
-    try:
-        #PS5   
-        store="falabella Col fisica"
-        temp_url = 'https://www.falabella.com.co/falabella-co/product/9440498/ps5-consola-standard/9440498'
-        test_url = 'https://www.falabella.com.co/falabella-co/product/9440500/Control-PS5-Dual-Sense/9440500'
-        # temp_url = test_url
-        temp_class = 'jsx-1816208196'
-        temp_id = 'button'
 
+#for x in range(15):
+while True:
+############################### CL
+    if True:
+        try:
+            ps5 = ps5_sarch_tool()
+            ps5.store = "zmart"
+            ps5.temp_url = 'https://www.zmart.cl/scripts/prodView.asp?idProduct=79725'
+            ps5.test_url = 'https://www.zmart.cl/scripts/prodView.asp?idProduct=79739'
+            
+            ps5.temp_class = 'comprar2015'
+            ps5.temp_id = 'input'
+
+            ps5.check_store()
+            resultado = ps5.results[0]
+            #print(resultado)
+            ps5.encontrada()
+        except:
+            print("not found in "+ps5.store)
+            ps5.driver.quit()
+            pass
+
+    if True:
+        try:
+            ps5 = ps5_sarch_tool()       
+            ps5.store = "ripley"  
+            ps5.temp_url = 'https://simple.ripley.cl/reserva-consola-ps5-digital-edition-2000380458314p'
+            ps5.test_url = 'https://simple.ripley.cl/control-ps5-dualsense-wireless-2000380632868p?color_80=blanco&cbar_widget_p4=&s=o'
+            # ps5.temp_url = test_url
+            ps5.temp_class = 'js-buy-button'
+            ps5.temp_id = 'button'
+            ps5.check_store()
+            #resultado = ps5.results[0]
+            #print(resultado)
+         
+            if ps5.results[0].next == 'Agregar a la bolsa':
+                ps5.encontrada()
+            else:
+                print("not found in "+ps5.store)
+                ps5.driver.quit()
+        except:
+            print("not found in "+ps5.store)
+            ps5.driver.quit()
+            pass
+
+    if True:
+        try:
+            ps5 = ps5_sarch_tool()  
+            ps5.store="microplay"
+            ps5.temp_url = 'https://www.microplay.cl/producto/consola-digital-ps5-sony/'
+            ps5.test_url = 'https://www.microplay.cl/producto/control-dualsense-ps5-wireless-white-sony/'
+            # ps5.temp_url = test_url
+            ps5.temp_class = 'btn-submit'
+            ps5.temp_id = 'a'
+            ps5.check_store()
+            resultado = ps5.results[0]
+            #print(resultado)
+            ps5.encontrada()
+        except:
+            print("not found in "+ps5.store)
+            ps5.driver.quit()
+            pass
+
+    if True:    
+        try:
+            ps5 = ps5_sarch_tool()   
+            ps5.store="Sony CL"
+            ps5.temp_url = 'https://store.sony.cl/playstation5-digital/p'
+            ps5.test_url = 'https://store.sony.cl/ps5-control-inalambrico-dualsense/p'
+            # ps5.temp_url = test_url
+            ps5.temp_class = 'new-add-to-cart'
+            ps5.temp_id = 'button'
+            ps5.check_store()
+            resultado = ps5.results[0]
+            #print(resultado)
+            ps5.encontrada()
+        except:
+            print("not found in "+ps5.store)
+            ps5.driver.quit()
+    
+    if True:
+        try:
+            ps5 = ps5_sarch_tool()  
+            ps5.store = "la polar"
+            ps5.temp_url ='https://www.lapolar.cl/consola-sony-playstation-5/23395401.html'
+            ps5.test_url = 'https://www.lapolar.cl/control-inalambrico-sony-ps5-dualsense-wireless-controller/23395355.html'
+            # ps5.temp_url = test_url
+            ps5.temp_class = 'add-to-cart'
+            ps5.temp_id = 'button'
+            ps5.check_store()
+            
+            #print(resultado)
+            if str(ps5.results[0].next).find("Agregar")>=0:
+                ps5.encontrada()
+            else:
+                print("not found in "+ps5.store)
+                ps5.driver.quit()
+        except:
+            print("not found in "+ps5.store)
+            ps5.driver.quit()
+    
+    if True:
+        try:
+            ps5 = ps5_sarch_tool()  
+            ps5.store = "Lider CL"
+            ps5.temp_url = 'https://www.lider.cl/catalogo/product/sku/1086920'
+            ps5.test_url = 'https://www.lider.cl/catalogo/product/sku/1086922'
+    
+            ps5.temp_class = 'walmart-add-cart-blue-button'
+            ps5.temp_id = 'div'
+            ps5.id='addProductToCart'
+            ps5.check_store()
+            resultado = ps5.results[0]
+            #print(resultado)
+            ps5.encontrada()
+        except:
+            print("not found in "+ps5.store)
+            ps5.driver.quit()
+    
+    if True:
+        try:
+            ps5 = ps5_sarch_tool()  
+            ps5.store = "PC factory"
+            ps5.temp_url = 'https://www.pcfactory.cl/producto/39192'
+            ps5.test_url = 'https://www.pcfactory.cl/producto/39193-sony-control-dualsense-ps5'
+    
+            ps5.temp_class = 'agregar_al_carro a_domicilio'
+            ps5.temp_id = 'a'
+            ps5.id='agrega_carro'
+            ps5.check_store()
+            resultado = ps5.results[0]
+            #print(resultado)
+            ps5.encontrada()
+        except:
+            print("not found in "+ps5.store)
+            ps5.driver.quit()
+
+
+############################### CO
+    if False:
+        try:
+            ps5 = ps5_sarch_tool()   
+            ps5.store="Sony CO Fisica"
+            ps5.temp_url = 'https://ps5.store.sony.com.co/playstation5/p'
+            ps5.test_url = 'https://ps5.store.sony.cl/ps5-control-inalambrico-dualsense/p'
+            # ps5.temp_url = test_url
+            ps5.temp_class = 'button'
+            ps5.temp_id = 'new-add-to-cart'
+            ps5.check_store()
+            resultado = ps5.results[0]
+            #print(resultado)
+            ps5.encontrada()
+        except:
+            print("not found in "+ps5.store)
+            ps5.driver.quit()
+
+    if False:
+        try:
+            ps5 = ps5_sarch_tool()  
+            ps5.store="falabella Col fisica"
+            ps5.temp_url = 'https://www.falabella.com.co/falabella-co/product/9440498/ps5-consola-standard/9440498'
+            ps5.test_url = 'https://www.falabella.com.co/falabella-co/product/9440500/Control-PS5-Dual-Sense/9440500'
+            # ps5.temp_url = test_url
+            ps5.temp_class = 'jsx-1816208196'
+            ps5.temp_id = 'button'
+            ps5.check_store()
+            resultado = ps5.results[0]
+            #print(resultado)
+            ps5.encontrada()
+        except:
+            print("not found in "+ps5.store)
+            ps5.driver.quit()
+    
+    if False:
+        try:
+            ps5 = ps5_sarch_tool()   
+            ps5.store="falabella Col fisica 2"
+            ps5.temp_url ='https://www.falabella.com.co/falabella-co/product/9440498/Consola-PlayStation-5-825GB/9440498'
+            ps5.test_url = 'https://www.falabella.com.co/falabella-co/product/9440500/Control-PS5-Dual-Sense/9440500'
+            # ps5.temp_url = test_url
+            ps5.temp_class = 'jsx-1816208196'
+            ps5.temp_id = 'button'
+            ps5.check_store()
+            resultado = ps5.results[0]
+            #print(resultado)
+            ps5.encontrada()
+        except:
+            print("not found in "+ps5.store)
+            ps5.driver.quit()
+    
+    if False:
+        try:
+            ps5 = ps5_sarch_tool()       
+            ps5.store = "falabella Col digital "
+            ps5.temp_url ='https://www.falabella.com.co/falabella-co/product/9440499/Consola-PlayStation-5-Digital-Edition-825GB/9440499'
+            ps5.test_url = 'https://www.falabella.com.co/falabella-co/product/9440500/Control-PS5-Dual-Sense/9440500'
+            # ps5.temp_url = test_url
+            ps5.temp_class = 'jsx-1816208196'
+            ps5.temp_id = 'button'
+            ps5.check_store()
+            resultado = ps5.results[0]
+            #print(resultado)
+            ps5.encontrada()
+        except:
+            print("not found in "+ps5.store)
+            ps5.driver.quit()
+    
         
-        results = check_store(temp_url,temp_class,temp_id)
-        resultado = results[0]
-        #print(resultado)
-        ps5_encontrada(temp_url,temp_class)
-
-    except:
-        print("not found in "+store)
-    try:
-        #PS5    
-        store="falabella Col fisica 2 "
-        temp_url ='https://www.falabella.com.co/falabella-co/product/9440498/Consola-PlayStation-5-825GB/9440498'
-        test_url = 'https://www.falabella.com.co/falabella-co/product/9440500/Control-PS5-Dual-Sense/9440500'
-        # temp_url = test_url
-        temp_class = 'jsx-1816208196'
-        temp_id = 'button'
-
-
-        results = check_store(temp_url,temp_class,temp_id)
-        resultado = results[0]
-        #print(resultado)
-        ps5_encontrada(temp_url,temp_class)
+    if False:
+        try:
+            ps5.store = "Pepe Ganga"
+            ps5.temp_url = 'https://www.pepeganga.com/consola-ps5-standard-711719541769/p?uam=true&mobile=4'
+            ps5.test_url = 'https://www.pepeganga.com/videojuego-ps5-call-of-duty-black-ops-cold-war-047875101142/p?idsku=117943&source_impresee=0a95b19b-d6b1-4260-8606-c08ed12089f3'
+            # ps5.temp_url = test_url
+            ps5.temp_class = 'buy-in-page-button'
+            ps5.temp_id = 'a'
+            ps5.check_store()
+            resultado = ps5.results[0]
+            #print(resultado)
+            ps5.encontrada()
+        except:
+            print("not found in "+ps5.store)
+            ps5.driver.quit()
         
-    except:
-        print("not found in "+store)
-    try:
-        #PS5  
-        store = "falabella Col digital "
-        temp_url ='https://www.falabella.com.co/falabella-co/product/9440499/Consola-PlayStation-5-Digital-Edition-825GB/9440499'
-        test_url = 'https://www.falabella.com.co/falabella-co/product/9440500/Control-PS5-Dual-Sense/9440500'
-        # temp_url = test_url
-        temp_class = 'jsx-1816208196'
-        temp_id = 'button'
-
-        
-        results = check_store(temp_url,temp_class,temp_id)
-        resultado = results[0]
-        #print(resultado)
-        ps5_encontrada(temp_url,temp_class)
-    except:
-        print("not found in "+store)
-    try:
-        #PS5   
-        store = "la polar"
-        temp_url ='https://www.lapolar.cl/consola-sony-playstation-5/23395401.html'
-        test_url = 'https://www.lapolar.cl/control-inalambrico-sony-ps5-dualsense-wireless-controller/23395355.html'
-        # temp_url = test_url
-        temp_class = 'add-to-cart'
-        temp_id = 'button'
+    
+       
 
 
-        results = check_store(temp_url,temp_class,temp_id)
-        resultado = results[0]
-        #print(resultado.next)
-        if str(resultado.next).find("Agregar")>=0:
-            ps5_encontrada(temp_url,temp_class)
-        else:
-            print("not found in "+store)
-    except:
-        print("not found in "+store)
-
-    try:
-        store = "Pepe Ganga"
-        temp_url = 'https://www.pepeganga.com/consola-ps5-standard-711719541769/p?uam=true&mobile=4'
-        test_url = 'https://www.pepeganga.com/videojuego-ps5-call-of-duty-black-ops-cold-war-047875101142/p?idsku=117943&source_impresee=0a95b19b-d6b1-4260-8606-c08ed12089f3'
-        # temp_url = test_url
-        temp_class = 'buy-in-page-button'
-        temp_id = 'a'
-
-
-        results = check_store(temp_url,temp_class,temp_id)
-        resultado = results[0]
-        #print(resultado.next)
-        ps5_encontrada(temp_url,temp_class)
-    except:
-        print("not found in "+store)
-
-
-   
